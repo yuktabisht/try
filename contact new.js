b22176d26b7bb470fb34c71badf5af33ceafc285
yuktabisht <yuktab2005@gmail.com>
2025-12-09 13:43:34 +0530

    Delete scroll-to-top button functionality
    Remove the JavaScript code for the scroll-to-top button functionality.

--- contact new.js
+++ contact new.js
@@ -1,11 +1,1 @@
-// JavaScript Document
 
-//up wala button
-  window.addEventListener("scroll", function () {
-    const upBtn = document.getElementById("up-btn");
-    if (window.scrollY > 300) {
-      upBtn.style.display = "block";
-    } else {
-      upBtn.style.display = "none";
-    }
-  });
